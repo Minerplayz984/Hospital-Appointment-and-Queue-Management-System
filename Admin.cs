5ba48fde3297c2918e6a54d2e72b939ed3f927f2
Ahmed Mortada <mohdmort2005@gmail,com>
2025-08-23 23:11:24 +0300

    Add project files.

---
+++ Admin.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital_Appointment_and_Queue_Management_System
+{
+    internal class Admin
+    {
+    }
+}
